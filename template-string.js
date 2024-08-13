82bbeeb3b29b2944c0dd713f80c2978ffc496604
MD Minarul Islam <wdminarul>
2024-08-13 10:30:29 +0600

    template-string

---
+++ template-string.js
@@ -0,0 +1,9 @@
+// template string using for dynamic data asign, mathmetical oparation
+const firstName = 'Shimul';
+const lastName = 'Islam'
+const number = 5;
+
+const fullName=`${firstName} ${lastName} ${number * 4}`
+console.log(fullName);
+
+//using backtik for template string.
