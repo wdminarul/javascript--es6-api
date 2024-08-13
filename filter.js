32dfbebe9bb0b0c6a92a566737437f332dfbc685
MD Minarul Islam <wdminarul>
2024-08-13 10:10:29 +0600

    filter

---
+++ filter.js
@@ -0,0 +1,15 @@
+// const person = ['shimul','joy','rakib','shakib'];
+
+// const filterName = person.filter((item)=>item === "rakib");
+// console.log(item);
+
+const newNumbers = [34,89,57,18,46,78];
+const filterNumber = newNumbers.filter((item)=>{
+    if(item == 18){
+        console.log('Quary Success');
+    }
+    else{
+        console.log('')
+    }
+}
+);
