8cbbdfbed97593b45c33e8173d9e14be68ab0bf1
MD Minarul Islam <wdminarul>
2024-08-13 10:34:14 +0600

    arrow-function

---
+++ arrow-function.js
@@ -0,0 +1,14 @@
+// function fullName(firstName,lastName="Islam"){
+//     const name = firstName + lastName;
+//     return name;
+// }
+
+// console.log(fullName('shimul', 'Rahman'))
+
+//aroow function, anonimus function
+
+const fullName = (firstName, lastName)=>{
+    const name = firstName + lastName;
+    return name;
+}
+console.log(fullName('shimul', 'Rahman'))
